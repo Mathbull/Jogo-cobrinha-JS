e11b605e4eda1257fb3c226dc377baa34826698e
MathBull <santos.matheus1012@gmail.com>
2023-07-14 14:43:39 -0300

    Definir direcoes e impedir que a cobrinha suma

--- script.js
+++ script.js
@@ -20,7 +20,23 @@ function criarCobrinha(){
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update(event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+    
+}
+
 function iniciarJogo(){
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16* box;
+    if(snake[0].y > 15* box && direction == "down") snake[0].y = 0;
+    if(snake [0].y < 0 && direction == "up") snake[0].y =16* box;
+
+
     criarBG();
     criarCobrinha(); 
 
@@ -28,18 +44,19 @@ function iniciarJogo(){
     let snakeY = snake[0].y;
 
     if(direction == "right") snakeX += box;
-    if(direction == "left") snakeY -= box;
+    if(direction == "left") snakeX -= box;
     if(direction == "up") snakeY -= box;
-    if(direction == "down") snake += box;
+    if(direction == "down") snakeY += box;
     
     snake.pop();
 
     let newHead = {
         x: snakeX,
-        y:snakeY
+        y: snakeY
     }
 
     snake.unshift(newHead)
 }
-let jogo = setInterval(iniciarJogo, 100)
+
+let jogo = setInterval(iniciarJogo, 200)
 
